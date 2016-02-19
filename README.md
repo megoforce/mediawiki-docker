7c307e21cbc46b59850c6b12fd780a65d8df35c6
explodi <phaser@tubatuba.net>
2016-02-18 21:20:51 -0300

    fix in admin username creation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mediawiki-docker
+Yet another Dockerfile for Mediawiki.
+
+# how to use this container
+TODO: Write this part (sorry)
--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 echo ${MYSQL_PORT_3306_TCP_ADDR}
 echo ${MYSQL_PORT_3306_TCP_PORT} 
 echo ${MYSQL_ENV_MYSQL_ROOT_PASSWORD}
-# mysql --host=${MYSQL_PORT_3306_TCP_ADDR} --port=${MYSQL_PORT_3306_TCP_PORT} -uroot -p${MYSQL_ENV_MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS mediawiki"
 mv -v /var/www/html/LocalSettings.php /var/www/html/LocalSettings.php.bak
-php maintenance/install.php --dbname mediawiki --dbserver ${MYSQL_PORT_3306_TCP_ADDR}:${MYSQL_PORT_3306_TCP_PORT} --dbpass ${MYSQL_ENV_MYSQL_ROOT_PASSWORD} --pass hackme ${WIKI_NAME} admin
+php maintenance/install.php --dbname mediawiki --dbserver ${MYSQL_PORT_3306_TCP_ADDR}:${MYSQL_PORT_3306_TCP_PORT} --dbpass ${MYSQL_ENV_MYSQL_ROOT_PASSWORD} --pass hackme "${WIKI_NAME}" admin
 mv -v /var/www/html/LocalSettings.php.bak /var/www/html/LocalSettings.php
